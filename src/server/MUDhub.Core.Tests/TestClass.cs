560f8191d1fa379125983b43ec268d2c18c61043
maSchoeller <git@marvinschoeller.de>
2020-03-31 00:25:03 +0100

    update

---
+++ src/server/MUDhub.Core.Tests/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MUDhub.Core.Tests
+{
+    public class TestClass
+    {
+        [Fact]
+        public void TestMethode()
+        {
+
+            Assert.True(true);
+
+        }
+    }
+}
