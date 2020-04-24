7e77680a3286571c4c834aacb08c7a14fc64a201
maSchoeller <git@marvinschoeller.de>
2020-04-24 10:33:30 +0200

    adding basic classes

---
+++ src/server/MUDhub.Core/Services/LoginService.cs
@@ -0,0 +1,11 @@
+﻿using MUDhub.Core.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MUDhub.Core.Services
+{
+    internal class LoginService : ILoginService
+    {
+    }
+}
