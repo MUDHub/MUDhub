f0806d9086324e2f9c47bb6d787096cbf401d330
maSchoeller <git@marvinschoeller.de>
2020-04-24 10:50:23 +0200

    adding dbContext

---
+++ src/server/MUDhub.Core/Services/MudDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MUDhub.Core.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MUDhub.Core.Services
+{
+    internal class MudDbContext : DbContext, IMudDbContext
+    {
+        public MudDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+    }
+}
