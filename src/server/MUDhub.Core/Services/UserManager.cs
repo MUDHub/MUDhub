f84e5a394105f2c8ea4f9e4da260c310a6eced96
KotschM <moris.kotsch@gmail.com>
2020-04-25 11:30:00 +0200

    Add Comments in UserManager.cs

--- src/server/MUDhub.Core/Services/UserManager.cs
+++ src/server/MUDhub.Core/Services/UserManager.cs
@@ -15,12 +15,16 @@ namespace MUDhub.Core.Services
     internal class UserManager : IUserManager
     {
 
-
         private readonly MudDbContext _context;
         private readonly ILogger? _logger;
         private readonly IEmailService _emailService;
 
-        
+        /// <summary>
+        /// Constructor for the UserManager.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="emailService"></param>
+        /// <param name="logger"></param>
         public UserManager(MudDbContext context, IEmailService emailService, ILogger<UserManager>? logger = null)
         {
             _context = context;
@@ -28,7 +32,11 @@ namespace MUDhub.Core.Services
             _emailService = emailService;
         }
 
-
+        /// <summary>
+        /// A user is registered asynchronously.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         public async Task<RegisterResult> RegisterUserAsync(RegistrationArgs model)
         {
             if (string.IsNullOrEmpty(model.Name) || 
@@ -60,7 +68,12 @@ namespace MUDhub.Core.Services
             }
             
         }
-        
+
+        /// <summary>
+        /// One user is removed asynchronously.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public async Task<bool> RemoveUserAsync(string userId)
         {
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId)
@@ -75,6 +88,12 @@ namespace MUDhub.Core.Services
             return true;
         }
 
+        /// <summary>
+        /// A role is added to the user asynchronously.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
         public async Task<bool> AddRoleToUserAsync(string userId, Roles role)
         {
             var user = await GetUserByIdAsync(userId).ConfigureAwait(false);
@@ -95,6 +114,12 @@ namespace MUDhub.Core.Services
             return true;
         }
 
+        /// <summary>
+        /// A role is asynchronously removed from the user.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
         public async Task<bool> RemoveRoleFromUserAsync(string userId, Roles role)
         {
             var user = await GetUserByIdAsync(userId).ConfigureAwait(false);
@@ -114,6 +139,13 @@ namespace MUDhub.Core.Services
             _logger?.LogInformation($"The role: '{role}' was removed from user: '{user.Name} {user.Lastname}'.");
             return true;
         }
+
+        /// <summary>
+        /// Checks asynchronously if the user has the role.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
         public async Task<bool> IsUserInRoleAsync(string userId, Roles role)
         {
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId).ConfigureAwait(false);
@@ -124,17 +156,34 @@ namespace MUDhub.Core.Services
             return ((user.Role & role) == role);
         }
 
+        /// <summary>
+        /// Sends an asynchronous email with the user's ResetKey.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
         public async Task<bool> GeneratePasswortResetAsync(string email)
         {
             return await _emailService.SendAsync(email, string.Empty).ConfigureAwait(false);
         }
 
+        /// <summary>
+        /// Asynchronously adds a new password to the user using the ResetKey.
+        /// </summary>
+        /// <param name="passwordresetkey"></param>
+        /// <param name="newPassword"></param>
+        /// <returns></returns>
         public Task<bool> UpdatePasswortFromResetAsync(string passwordresetkey, string newPassword)
         {
-            
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Overwrites the old password of the user asynchronously with a new one.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="oldPassword"></param>
+        /// <param name="newPassword"></param>
+        /// <returns></returns>
         public async Task<bool> UpdatePasswortAsync(string userId, string oldPassword, string newPassword)
         {
             var user = await GetUserByIdAsync(userId).ConfigureAwait(false);
@@ -154,6 +203,11 @@ namespace MUDhub.Core.Services
             return true;
         }
 
+        /// <summary>
+        /// A password hash is generated from the password.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
         private string CreatePasswordHash(string password)
         {
             byte[] data;
@@ -171,6 +225,12 @@ namespace MUDhub.Core.Services
             });
             return passwordHash;
         }
+
+        /// <summary>
+        /// Get the user asynchronously using the UserID.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         private async Task<User> GetUserByIdAsync(string userId)
         {
             return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId).ConfigureAwait(false);
