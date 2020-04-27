4b0bab682d53c01a18f4995599af8c60c5357f16
Paul Finkbeiner <paul-d-f@web.de>
2020-04-27 22:53:00 +0200

    small changes to create mud routing module

--- src/client/src/app/configuration/create-mud/create-mud-routing.module.ts
+++ src/client/src/app/configuration/create-mud/create-mud-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
 				component: GeneralComponent,
 				children: [
 					{
-						path: 'aria-list',
+						path: 'area-list',
 						component: AreaListComponent
 					},
 					{
