94699d8733798c6f93c31815fc826d1cc255c22b
Mario Lang <mariolang1@gmx.de>
2020-04-30 11:10:32 +0200

    restructered chat service added new message observables

--- src/client/src/app/services/chat.service.ts
+++ src/client/src/app/services/chat.service.ts
@@ -1,42 +1,45 @@
 import { Injectable } from '@angular/core';
 import { IMessage } from '../model/IMessage';
+import { Subject } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class ChatService {
-	constructor() {}
 
-	private _histories: {
-		room: IMessage[],
-		global: IMessage[]
-	} = {
-		global: [
-			{
-				content: 'Hello global world!',
-				sender: 'Mario Lang'
-			},
-			{
-				content: 'Hello global world!',
-				sender: 'Mario Lang'
-			},
-			{
-				content: 'Hello global world!',
-				sender: 'Mario Lang'
-			},
-		],
-		room: [
-			{
-				content: 'Hello room world!',
-				sender: 'Mario Lang'
-			},
-		]
-	};
+	private NewGlobalMessageSubject = new Subject<IMessage>();
+	public NewGlobalMessage$ = this.NewGlobalMessageSubject.asObservable();
+
+	private NewRoomMessageSubject = new Subject<IMessage>();
+	public NewRoomMessage$ = this.NewRoomMessageSubject.asObservable();
+
+
+	private _globalHistory: IMessage[] = [
+		{
+			content: 'Hello global world!',
+			sender: 'Mario Lang',
+		},
+		{
+			content: 'Hello global world!',
+			sender: 'Mario Lang',
+		},
+		{
+			content: 'Hello global world!',
+			sender: 'Mario Lang',
+		},
+	];
+
+	private _roomHistory: IMessage[] = [
+		{
+			content: 'Hello room world!',
+			sender: 'Mario Lang',
+		},
+	];
 
 	public get globalHistory() {
-		return this._histories.global;
+		return this._globalHistory;
 	}
 	public get roomHistory() {
-		return this._histories.room;
+		return this._roomHistory;
 	}
 }
