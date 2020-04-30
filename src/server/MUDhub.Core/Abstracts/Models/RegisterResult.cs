1a14735d5dc578efd97b81ced8837164ea1257b7
Moris Kotsch <48104013+KotschM@users.noreply.github.com>
2020-04-30 15:52:02 +0200

    add Usermanagement Services (#48)
    * First steps for Usermanagement
    
    * add async programming model
    
    * add basic unittests
    
    * update
    
    * Add more Tests for UserManager
    
    * add EMailService
    
    * bugfix in Test
    
    * improvements Tests
    
    * Add Comments in UserManager.cs
    
    * improve tests
    
    * Create client_pullrequest.yml
    
    * Delete client_pullrequest.yml
    
    * add tests for UserManager
    
    * update
    
    * fix merging from develop
    
    * update
    
    * update
    
    * Email Shit
    
    * update
    
    * fix mail service
    
    * implement LoginService And Tests
    
    * start api entpoint implementation
    
    * adding databaseinitializer
    
    * bugif
    
    * fix test
    
    * login über api
    
    * fix database
    
    * add migrations
    
    * implement register API
    
    * update
    
    * Add API for Password Reset
    
    * fix  test
    
    Co-authored-by: KotschM <moris.kotsch@gmail.com>
    Co-authored-by: maSchoeller <git@marvinschoeller.de>

---
+++ src/server/MUDhub.Core/Abstracts/Models/RegisterResult.cs
@@ -0,0 +1,21 @@
+﻿using MUDhub.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MUDhub.Core.Abstracts.Models
+{
+    public class RegisterResult
+    {
+
+        public RegisterResult(bool succeeded, bool usernameAlreadyExists = false, User? user = null)
+        {
+            Succeeded = succeeded;
+            UsernameAlreadyExists = usernameAlreadyExists;
+            User = user;
+        }
+        public bool Succeeded { get; }
+        public bool UsernameAlreadyExists { get; }
+        public User? User{ get; set; }
+    }
+}
--- src/server/MUDhub.Core/Services/UserManager.cs
+++ src/server/MUDhub.Core/Services/UserManager.cs
@@ -1,11 +1,243 @@
 ﻿using MUDhub.Core.Abstracts;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using MUDhub.Core.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MUDhub.Core.Abstracts.Models;
+using MUDhub.Core.Helper;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MUDhub.Core.Services
-{ 
+{
     internal class UserManager : IUserManager
     {
+
+        private readonly MudDbContext _context;
+        private readonly ILogger? _logger;
+        private readonly IEmailService _emailService;
+
+        /// <summary>
+        /// Constructor for the UserManager.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="emailService"></param>
+        /// <param name="logger"></param>
+        public UserManager(MudDbContext context, IEmailService emailService, ILogger<UserManager>? logger = null)
+        {
+            _context = context;
+            _logger = logger;
+            _emailService = emailService;
+        }
+
+        /// <summary>
+        /// A user is registered asynchronously.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public async Task<RegisterResult> RegisterUserAsync(RegistrationArgs model)
+        {
+            if (string.IsNullOrWhiteSpace(model.Name) ||
+                string.IsNullOrWhiteSpace(model.Email) ||
+                string.IsNullOrWhiteSpace(model.Password))
+            {
+                return new RegisterResult(false);
+            }
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == model.Email)
+                .ConfigureAwait(false);
+            if (user == null)
+            {
+                var newUser = new User
+                {
+                    Name = model.Name,
+                    Lastname = model.Lastname,
+                    Email = model.Email,
+                    PasswordHash = UserHelpers.CreatePasswordHash(model.Password)
+                };
+                await _context.AddAsync(newUser);
+                await _context.SaveChangesAsync()
+                    .ConfigureAwait(false);
+                return new RegisterResult(true, user: newUser);
+            }
+            else
+            {
+                return new RegisterResult(false, true);
+            }
+
+        }
+
+        /// <summary>
+        /// One user is removed asynchronously.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task<bool> RemoveUserAsync(string userId)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId)
+                .ConfigureAwait(false);
+
+            if (user == null) return false;
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync()
+                .ConfigureAwait(false);
+
+            _logger?.LogInformation($"User: '{user.Name} {user.Lastname}' is removed.");
+            return true;
+        }
+
+        /// <summary>
+        /// A role is added to the user asynchronously.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public async Task<bool> AddRoleToUserAsync(string userId, Roles role)
+        {
+            var user = await GetUserByIdAsync(userId).ConfigureAwait(false);
+            if (user == null)
+            {
+                _logger?.LogWarning($"The role: '{role}' could not be added to user: '{user?.Name} {user?.Lastname}'. => User does not exist");
+                return false;
+            }
+            if ((user.Role & role) == role)
+            {
+                _logger?.LogWarning($"The role: '{role}' could not be added to user: '{user.Name} {user.Lastname}'. => User already has the role");
+                return false;
+            }
+            user.Role |= role;
+            await _context.SaveChangesAsync()
+                .ConfigureAwait(false);
+            _logger?.LogInformation($"The role: '{role}' was added to user: '{user.Name} {user.Lastname}'.");
+            return true;
+        }
+
+        /// <summary>
+        /// A role is asynchronously removed from the user.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public async Task<bool> RemoveRoleFromUserAsync(string userId, Roles role)
+        {
+            var user = await GetUserByIdAsync(userId).ConfigureAwait(false);
+            if (user == null)
+            {
+                _logger?.LogWarning($"The role: '{role}' could not be removed from user: '{user?.Name} {user?.Lastname}'. => User does not exist");
+                return false;
+            }
+            if ((user.Role & role) != role)
+            {
+                _logger?.LogWarning($"The role: '{role}' could not be removed from user: '{user.Name} {user.Lastname}'. => User has not the role");
+                return false;
+            }
+            user.Role &= ~role;
+            await _context.SaveChangesAsync()
+                .ConfigureAwait(false);
+            _logger?.LogInformation($"The role: '{role}' was removed from user: '{user.Name} {user.Lastname}'.");
+            return true;
+        }
+
+        /// <summary>
+        /// Checks asynchronously if the user has the role.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public async Task<bool> IsUserInRoleAsync(string userId, Roles role)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId).ConfigureAwait(false);
+            return IsUserInRole(user, role);
+        }
+
+        /// <summary>
+        /// Sends an asynchronous email with the user's ResetKey.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public async Task<bool> GeneratePasswortResetAsync(string email)
+        {
+            return await _emailService.SendAsync(email, string.Empty).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Asynchronously adds a new password to the user using the ResetKey.
+        /// </summary>
+        /// <param name="passwordresetkey"></param>
+        /// <param name="newPassword"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdatePasswortFromResetAsync(string passwordresetkey, string newPassword)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.PasswordResetKey == passwordresetkey)
+                .ConfigureAwait(false);
+            if (user == null)
+            {
+                _logger?.LogWarning($"The Password of '{user?.Name} {user?.Lastname}' could not be reseted. => User does not exist.");
+                return false;
+            }
+
+            var newPasswordHash = UserHelpers.CreatePasswordHash(newPassword);
+
+            if (user.PasswordHash == newPasswordHash)
+            {
+                _logger?.LogWarning($"The Password of '{user.Name} {user.Lastname}' could not be reseted. => Old password same as new password.");
+                return false;
+            }
+
+            user.PasswordHash = newPasswordHash;
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+            _logger?.LogInformation($"The Password of '{user.Name} {user.Lastname}' was reseted.");
+            return true;
+        }
+
+        /// <summary>
+        /// Overwrites the old password of the user asynchronously with a new one.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="oldPassword"></param>
+        /// <param name="newPassword"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdatePasswortAsync(string userId, string oldPassword, string newPassword)
+        {
+            var user = await GetUserByIdAsync(userId).ConfigureAwait(false);
+            if (user == null)
+            {
+                _logger?.LogWarning($"The Password of '{user?.Name} {user?.Lastname}' could not be updated. => User does not exist.");
+                return false;
+            }
+            if (oldPassword == newPassword)
+            {
+                _logger?.LogWarning($"The Password of '{user.Name} {user.Lastname}' could not be updated. => Old password same as new password.");
+                return false;
+            }
+            if (UserHelpers.CreatePasswordHash(oldPassword) != user.PasswordHash)
+            {
+                _logger?.LogWarning($"The Password of '{user.Name} {user.Lastname}' could not be updated. => Old password is not the actual password.");
+                return false;
+            }
+
+            user.PasswordHash = UserHelpers.CreatePasswordHash(newPassword);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+            _logger?.LogInformation($"The Password of '{user.Name} {user.Lastname}' was updated.");
+            return true;
+        }
+
+
+        /// <summary>
+        /// Get the user asynchronously using the UserID.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        private async Task<User> GetUserByIdAsync(string userId)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId).ConfigureAwait(false);
+        }
+
+        public bool IsUserInRole(User user, Roles role)
+        {
+            if (user is null)
+            {
+                return false;
+            }
+            return ((user.Role & role) == role);
+        }
     }
 }
