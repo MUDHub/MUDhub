94636f04207099fe36a167314868af8cb7628077
Mario Lang <mariolang1@gmx.de>
2020-05-01 11:11:36 +0200

    removed unnecessary components

--- src/client/src/app/configuration/configuration-routing.module.ts
+++ src/client/src/app/configuration/configuration-routing.module.ts
@@ -2,35 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ConfigurationComponent } from './configuration.component';
-import { CreatedGamesComponent } from './created-games/created-games.component';
-import { EditMudComponent } from './edit-mud/edit-mud.component';
 
 const routes: Routes = [
 	{
 		path: '',
-		component: ConfigurationComponent,
-		children: [
-			{
-				path: '',
-				redirectTo: 'my-muds',
-				pathMatch: 'full',
-			},
-			{
-				path: 'my-muds',
-				component: CreatedGamesComponent,
-			},
-			{
-				path: 'edit',
-				component: EditMudComponent,
-			},
-			{
-				path: 'create',
-				loadChildren: () =>
-					import('./create-mud/create-mud.module').then(
-						(m) => m.CreateMudModule
-					),
-			},
-		],
+		component: ConfigurationComponent
 	},
 ];
 
