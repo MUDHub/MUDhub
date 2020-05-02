709f7ece25f85c9433e592413648c48c1aad1fde
Mario Lang <mariolang1@gmx.de>
2020-05-02 12:33:25 +0200

    typed environment variables

--- src/client/src/environments/environment.ts
+++ src/client/src/environments/environment.ts
@@ -2,8 +2,13 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import { IEnviromnent } from './IEnvironment';
+
+export const environment: IEnviromnent = {
+	production: false,
+	api: {
+		path: 'http://localhost:5000/api/'
+	}
 };
 
 /*
