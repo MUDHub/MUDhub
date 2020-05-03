67dbd41ae66801e9b01e0043c6b301e048d79ad3
Paul Finkbeiner <paul-d-f@web.de>
2020-05-03 17:41:00 +0200

    emptied array

--- src/client/src/app/home/my-muds/my-muds.component.ts
+++ src/client/src/app/home/my-muds/my-muds.component.ts
@@ -11,32 +11,7 @@ export class MyMudsComponent implements OnInit {
 
 	constructor(private mudService: MudService, private authService: AuthService) { }
 
-	muds: IMud[] = [
-		{
-			mudId: "ajsdlksajdl",
-			name: "Mud1",
-			description: "description",
-			isPublic: "true",
-			autoRestart: true,
-			owner: this.authService.user
-		},
-		{
-			mudId: "ajsdlksajdl",
-			name: "Mud2",
-			description: "description",
-			isPublic: "true",
-			autoRestart: true,
-			owner: this.authService.user
-		},
-		{
-			mudId: "ajsdlksajdl",
-			name: "Mud3",
-			description: "description",
-			isPublic: "true",
-			autoRestart: true,
-			owner: this.authService.user
-		}
-	];
+	muds: IMud[] = [];
 
 	async ngOnInit() {
 		//this.muds = await this.mudService.getById(this.authService.user.id);
