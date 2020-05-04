0648de241044ba0cf060ed9783e0d35c67059fdf
langmario <mariolang1@gmx.de>
2020-05-04 21:48:51 +0200

    added shared module for components

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { NotFoundComponent } from './not-found.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
 	declarations: [AppComponent, NotFoundComponent],
@@ -15,6 +16,7 @@ import { NotFoundComponent } from './not-found.component';
 		AppRoutingModule,
 		HttpClientModule,
 		BrowserAnimationsModule,
+		SharedModule,
 	],
 	providers: [
 		{
---
+++ src/client/src/app/shared/exit-button/exit-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+	selector: 'mh-exit-button',
+	templateUrl: './exit-button.component.html',
+	styleUrls: ['./exit-button.component.scss'],
+})
+export class ExitButtonComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
