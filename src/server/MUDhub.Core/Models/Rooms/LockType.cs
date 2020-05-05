d6863437d13c857687486c8aef729e87d07004fd
KotschM <moris.kotsch@gmail.com>
2020-05-05 10:13:14 +0200

    add Models for Room and Area #EpsteinDidntKillHimself

---
+++ src/server/MUDhub.Core/Models/Rooms/LockType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MUDhub.Core.Models.Rooms
+{
+    public enum LockType
+    {
+        ItemLock,
+        InteractionLock,
+        CharacterRaceLock,
+        CharacterClassLock,
+        NoLock
+    }
+}
---
+++ src/server/MUDhub.Core/Models/Rooms/RoomConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MUDhub.Core.Models.Rooms
+{
+    public class RoomConnection
+    {
+
+        public RoomConnection(string roomConnectionId)
+        {
+            Id = roomConnectionId;
+        }
+
+        public string Id { get; }
+
+        public string Description { get; set; } = string.Empty;
+        public Room Room1 { get; set; } = new Room();
+        public Room Room2 { get; set; } = new Room();
+
+        //Todo: Was ist der Default-Wert für diese Property?
+        public ConnectionDirection Direction { get; set; } = ConnectionDirection.North;
+
+        //Todo: Was ist der Default-Wert für diese Property?
+        public LockType LockType { get; set; } = LockType.NoLock;
+
+        public string LockDescription { get; set; } = string.Empty;
+        public string LockAssociatedId { get; set; } = string.Empty;
+    }
+}
