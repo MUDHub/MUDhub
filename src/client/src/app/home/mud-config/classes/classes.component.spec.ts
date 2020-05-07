0dd9bf45ca8a259ae74fc89224070325be977b97
langmario <mariolang1@gmx.de>
2020-05-07 09:41:17 +0200

    fixed tests

--- src/client/src/app/home/mud-config/classes/classes.component.spec.ts
+++ src/client/src/app/home/mud-config/classes/classes.component.spec.ts
@@ -3,23 +3,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ClassesComponent } from './classes.component';
 
 describe('ClassesComponent', () => {
-  let component: ClassesComponent;
-  let fixture: ComponentFixture<ClassesComponent>;
+	let component: ClassesComponent;
+	let fixture: ComponentFixture<ClassesComponent>;
 
-  beforeEach(async(() => {
-	TestBed.configureTestingModule({
-		declarations: [ ClassesComponent ]
-	})
-	.compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ClassesComponent],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-	fixture = TestBed.createComponent(ClassesComponent);
-	component = fixture.componentInstance;
-	fixture.detectChanges();
-  });
-
-  it('should create', () => {
-	expect(component).toBeTruthy();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ClassesComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 });
--- src/client/src/app/home/mud-config/races/races.component.spec.ts
+++ src/client/src/app/home/mud-config/races/races.component.spec.ts
@@ -3,23 +3,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RacesComponent } from './races.component';
 
 describe('RacesComponent', () => {
-  let component: RacesComponent;
-  let fixture: ComponentFixture<RacesComponent>;
+	let component: RacesComponent;
+	let fixture: ComponentFixture<RacesComponent>;
 
-  beforeEach(async(() => {
-	TestBed.configureTestingModule({
-		declarations: [ RacesComponent ]
-	})
-	.compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [RacesComponent],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-	fixture = TestBed.createComponent(RacesComponent);
-	component = fixture.componentInstance;
-	fixture.detectChanges();
-  });
-
-  it('should create', () => {
-	expect(component).toBeTruthy();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(RacesComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 });
