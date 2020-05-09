3e8578fcdb31cd4eb9f7cdf41ac7d16e899e5c67
KotschM <moris.kotsch@gmail.com>
2020-05-09 23:40:44 +0200

    Add InventoryController without implementation

---
+++ src/server/MUDhub.Server/Controllers/InventoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MUDhub.Core.Abstracts;
+using MUDhub.Core.Services;
+
+namespace MUDhub.Server.Controllers
+{
+    [Route("api/muds/{mudid}/areas/{areaid}/rooms/{roomid}")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly MudDbContext _context;
+        private readonly IInventoryService _inventoryService;
+
+        public InventoryController(MudDbContext context, IInventoryService inventoryService)
+        {
+            _context = context;
+            _inventoryService = inventoryService;
+        }
+    }
+}
