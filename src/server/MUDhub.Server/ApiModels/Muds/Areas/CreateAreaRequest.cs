1f2bf470b124bbaf0c643038541ce24e69d0077a
langmario <mariolang1@gmx.de>
2020-05-09 16:09:45 +0200

    made area description optional

--- src/server/MUDhub.Server/ApiModels/Muds/Areas/CreateAreaRequest.cs
+++ src/server/MUDhub.Server/ApiModels/Muds/Areas/CreateAreaRequest.cs
@@ -7,7 +7,6 @@ namespace MUDhub.Server.ApiModels.Muds.Areas
     {
         [Required]
         public string Name { get; set; } = string.Empty;
-        [Required]
         public string Description { get; set; } = string.Empty;
 
         public static AreaArgs ConvertFromRequest(CreateAreaRequest request)
