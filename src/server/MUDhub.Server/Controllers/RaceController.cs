4f118e29edefbeb33807927b03eb62689321961b
maSchoeller <git@marvinschoeller.de>
2020-05-10 18:38:30 +0200

    fix

--- src/server/MUDhub.Server/Controllers/RaceController.cs
+++ src/server/MUDhub.Server/Controllers/RaceController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace MUDhub.Server.Controllers
 {
-    [Route("api/mudgame/{mudid}/Races")]
+    [Route("api/mudgame/{mudid}/races")]
     [ApiController]
     public class RacesController : ControllerBase
     {
