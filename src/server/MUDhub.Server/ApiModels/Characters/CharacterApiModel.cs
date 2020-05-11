8561300e7a5cef382db42f6f77f2f4e10ef8a90f
maSchoeller <git@marvinschoeller.de>
2020-05-11 11:08:36 +0200

    update character api model

--- src/server/MUDhub.Server/ApiModels/Characters/CharacterApiModel.cs
+++ src/server/MUDhub.Server/ApiModels/Characters/CharacterApiModel.cs
@@ -8,12 +8,13 @@ namespace MUDhub.Server.ApiModels.Characters
 {
     public class CharacterApiModel
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string OwnerId { get; set; }
-        public string OwnerFullname { get; set; }
-        public string Race { get; set; }
-        public string Class { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string OwnerId { get; set; } = string.Empty;
+        public string OwnerFullname { get; set; } = string.Empty;
+        public string RaceName { get; set; } = string.Empty;
+        public string ClassName { get; set; } = string.Empty;
+        public string RoomName { get; set; } = string.Empty;
 
 
         public static CharacterApiModel FromCharacter(Character character)
@@ -25,7 +26,9 @@ namespace MUDhub.Server.ApiModels.Characters
             {
                 Id = character.Id,
                 Name = character.Name,
-                Class = character.Class.Name,
+                ClassName = character.Class.Name,
+                RaceName = character.Race.Name,
+                RoomName = character.ActualRoom.Name,
                 OwnerFullname = character.Owner.Name + " " +character.Owner.Lastname,
             };
         }
